7f581e6426483992c461acef8949b2c7b0a4b362
github username <github email address>
2017-04-20 16:03:40 -0400

    changes

--- app/models/official.rb
+++ app/models/official.rb
@@ -2,6 +2,7 @@ class Official < ActiveRecord::Base
   belongs_to :state
 
   def self.display_official_info(choice)
+
     chosen_official = self.find_by(name: choice)
 
     self.get_columns_without_id.map do |attribute|
@@ -36,14 +37,16 @@ class Official < ActiveRecord::Base
     officials = all_state_officials_names(api_hash)
     officials_hash = get_official_hash(officials, api_hash, address)
 
-    officials_hash.each do |official_hash|
+    officials_hash.each.with_index(1) do |official_hash, i|
       new_official = Official.find_or_create_by(official_hash)
       index_of_official = (all_state_officials_names(api_hash)).index(official_hash[:name])
       office_id = Office.get_official_id(index_of_official, api_hash)
 
       OfficeOfficial.find_or_create_by(official_id: new_official.id, office_id: office_id)
 
-      puts "#{Office.find_by(id: office_id).position} : #{official_hash[:name]}"
+      puts "----------------------------------------------------------------------------".blue
+      puts "#{i}.)   #{official_hash[:name]} (#{Office.find_by(id: office_id).position})"
+      officials_hash
     end
   end
 
